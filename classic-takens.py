0cd18ebc5e8b51ff7511bb0592e4b62e6ea7e4a6
sandman <bameneyr@vols.utk.edu>
2022-04-20 19:23:43 -0400

    Finished and tested kcomplex function.

--- classic-takens.py
+++ classic-takens.py
@@ -3,60 +3,61 @@ import matplotlib.pyplot as plt
 from numpy import pi
 from numpy import linalg as LA
 from math import comb as nCk
+from itertools import combinations
 
 
 ##############
 # Parameters #
 ##############
-T = 9 # Number of data-times
-tau = 1 # Delay
-d = 2 # Dimension of point cloud
-eps1 = 0.1 # First scale
-eps2 = 0.4 # Second scale
-k = 1 # Dimension for Betti number
-xi = 1.0 # Parameter for Dirac operator
-def f(x): return np.sin((2.0*pi)*x) # Time series function
+#T = 9 # Number of data-times
+#tau = 1 # Delay
+#d = 2 # Dimension of point cloud
+#eps1 = 0.1 # First scale
+#eps2 = 0.4 # Second scale
+#k = 1 # Dimension for Betti number
+#xi = 1.0 # Parameter for Dirac operator
+#def f(x): return np.sin((2.0*pi)*x) # Time series function
 
 
 #####################
 # Values used often #
 #####################
-points = T - (tau*(d-1)) # Number of points
-time = np.linspace(0.0, 1.0, num=T, endpoint=True) # Time series times
-series = f(time) # Time series
-cloudx = series[:points] # Point Cloud x
-cloudy = series[tau:] # Point Cloud y
+#points = T - (tau*(d-1)) # Number of points
+#time = np.linspace(0.0, 1.0, num=T, endpoint=True) # Time series times
+#series = f(time) # Time series
+#cloudx = series[:points] # Point Cloud x
+#cloudy = series[tau:] # Point Cloud y
 
 
 ########################
 # Plotting Time series #
 ########################
-fig1 = plt.figure()
-plt.plot(time, series, 'o')
-plt.ylim(-1.2,1.2)
-plt.xlim(-0.2, 1.2)
-plt.title("Time Series", size = 24, weight = 'bold')
-3plt.xlabel("time")
-plt.ylabel("sine")
+#fig1 = plt.figure()
+#plt.plot(time, series, 'o')
+#plt.ylim(-1.2,1.2)
+#plt.xlim(-0.2, 1.2)
+#plt.title("Time Series", size = 24, weight = 'bold')
+#plt.xlabel("time")
+#plt.ylabel("sine")
 
 
 ########################
 # Plotting Point Cloud #
 ########################
-fig2 = plt.figure()
-plt.plot(cloudx, cloudy, 'o')
-plt.ylim(-1.2,1.2)
-plt.xlim(-1.2, 1.2)
-plt.title("Point Cloud", size = 24, weight = 'bold')
-plt.xlabel("x(t)")
-plt.ylabel("x(t + tau)")
+#fig2 = plt.figure()
+#plt.plot(cloudx, cloudy, 'o')
+#plt.ylim(-1.2, 1.2)
+#plt.xlim(-1.2, 1.2)
+#plt.title("Point Cloud", size = 24, weight = 'bold')
+#plt.xlabel("x(t)")
+#plt.ylabel("x(t + tau)")
 
 
 ################
 # Saving plots #
 ################
-fig1.savefig("figures/time-series.png")
-fig2.savefig("figures/point-cloud.png")
+#fig1.savefig("figures/time-series.png")
+#fig2.savefig("figures/point-cloud.png")
 
 
 #################
@@ -75,7 +76,9 @@ fig2.savefig("figures/point-cloud.png")
 
 # k-simplices
 def kcomplex(k, n):
-    comp = np.zeros( (nCk(n,k), n) ) # Write simplices as lists of 0s and 1s ####################
+    comp = np.zeros( (nCk(n,k), n) )
+    for row, combo in enumerate( np.array( list( combinations( range(n), k) ) ) ):
+        comp[row][combo] = 1
     return comp
 
 
@@ -168,5 +171,7 @@ print(f"Test 4 is {test4} and should be 0")
 test5 = isface(simp4, simp1)
 print(f"Test 5 is {test5} and should be 0")
 
+test6 = kcomplex(2, 4)
+print(f"Test 6 is \n {test6}")
 
 
