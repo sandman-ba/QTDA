fbe24f2c46563e0fbd701760a4358bd62b9b11bc
sandman <bameneyr@vols.utk.edu>
2022-04-20 20:50:06 -0400

    Tested code on sine time series.

--- classic-takens.py
+++ classic-takens.py
@@ -6,69 +6,6 @@ from math import comb as nCk
 from itertools import combinations
 
 
-##############
-# Parameters #
-##############
-#T = 9 # Number of data-times
-#tau = 1 # Delay
-#d = 2 # Dimension of point cloud
-#eps1 = 0.1 # First scale
-#eps2 = 0.4 # Second scale
-#k = 1 # Dimension for Betti number
-#xi = 1.0 # Parameter for Dirac operator
-#def f(x): return np.sin((2.0*pi)*x) # Time series function
-
-
-#####################
-# Values used often #
-#####################
-#points = T - (tau*(d-1)) # Number of points
-#time = np.linspace(0.0, 1.0, num=T, endpoint=True) # Time series times
-#series = f(time) # Time series
-#cloudx = series[:points] # Point Cloud x
-#cloudy = series[tau:] # Point Cloud y
-
-
-########################
-# Plotting Time series #
-########################
-#fig1 = plt.figure()
-#plt.plot(time, series, 'o')
-#plt.ylim(-1.2,1.2)
-#plt.xlim(-0.2, 1.2)
-#plt.title("Time Series", size = 24, weight = 'bold')
-#plt.xlabel("time")
-#plt.ylabel("sine")
-
-
-########################
-# Plotting Point Cloud #
-########################
-#fig2 = plt.figure()
-#plt.plot(cloudx, cloudy, 'o')
-#plt.ylim(-1.2, 1.2)
-#plt.xlim(-1.2, 1.2)
-#plt.title("Point Cloud", size = 24, weight = 'bold')
-#plt.xlabel("x(t)")
-#plt.ylabel("x(t + tau)")
-
-
-################
-# Saving plots #
-################
-#fig1.savefig("figures/time-series.png")
-#fig2.savefig("figures/point-cloud.png")
-
-
-#################
-# Betti numbers #
-#################
-#dirop = dirac(k, n, eps1, eps2, xi) # Dirac operator
-#eigen, _ = LA.eig( dirop ) # Eigenvalues and eigenvectors of dirac operator
-#betti = np.sum( np.absolute(eigen - 1.0) < 0.001 ) # Multiplicity of eigenvalue 1
-#print(f"The number of loops that persist from scale {eps1} to scale {eps2} is:\n {betti}")
-
-
 ##############################
 #        Functions           #
 ##############################
@@ -76,8 +13,8 @@ from itertools import combinations
 
 # k-simplices
 def kcomplex(k, n):
-    comp = np.zeros( (nCk(n,k), n) )
-    for row, combo in enumerate( np.array( list( combinations( range(n), k) ) ) ):
+    comp = np.zeros( (nCk(n,k+1), n) )
+    for row, combo in enumerate( np.array( list( combinations( range(n), k+1) ) ) ):
         comp[row][combo] = 1
     return comp
 
@@ -145,6 +82,80 @@ def dirac(k, n, x, y, eps1, eps2, xi):
     return di
 
 
+##############
+# Parameters #
+##############
+T = 9 # Number of data-times
+tau = 1 # Delay
+d = 2 # Dimension of point cloud
+e1 = 1.0 # First scale
+e2 = 1.3 # Second scale
+e3 = 2.0 # Third scale
+betk = 1 # Dimension for Betti number
+xi = 1.0 # Parameter for Dirac operator
+def f(x): return np.sin((2.0*pi)*x) # Time series function
+
+
+#####################
+# Values used often #
+#####################
+points = T - (tau*(d-1)) # Number of points
+time = np.linspace(0.0, 1.0, num=T, endpoint=True) # Time series times
+series = f(time) # Time series
+cloudx = series[:points] # Point Cloud x
+cloudy = series[tau:] # Point Cloud y
+
+
+#################
+# Betti numbers #
+#################
+dirop = dirac(betk, points, cloudx, cloudy, e1, e2, xi) # Dirac operator
+eigen, _ = LA.eig( dirop ) # Eigenvalues and eigenvectors of dirac operator
+betti = np.sum( np.absolute(eigen - 1.0) < 0.001 ) # Multiplicity of eigenvalue 1
+print(f"The number of loops that persist from scale {e1} to scale {e2} is:\n {betti}")
+
+dirop2 = dirac(betk, points, cloudx, cloudy, e2, e2, xi) # Dirac operator
+eigen2, _ = LA.eig( dirop2 ) # Eigenvalues and eigenvectors of dirac operator
+betti2 = np.sum( np.absolute(eigen2 - 1.0) < 0.001 ) # Multiplicity of eigenvalue 1
+print(f"The number of loops that at scale {e2} is:\n {betti2}")
+
+dirop3 = dirac(betk, points, cloudx, cloudy, e2, e3, xi) # Dirac operator
+eigen3, _ = LA.eig( dirop3 ) # Eigenvalues and eigenvectors of dirac operator
+betti3 = np.sum( np.absolute(eigen3 - 1.0) < 0.001 ) # Multiplicity of eigenvalue 1
+print(f"The number of loops that persist from scale {e2} to scale {e3} is:\n {betti3}")
+
+
+########################
+# Plotting Time series #
+########################
+fig1 = plt.figure()
+plt.plot(time, series, 'o')
+plt.ylim(-1.2,1.2)
+plt.xlim(-0.2, 1.2)
+plt.title("Time Series", size = 24, weight = 'bold')
+plt.xlabel("time")
+plt.ylabel("sine")
+
+
+########################
+# Plotting Point Cloud #
+########################
+fig2 = plt.figure()
+plt.plot(cloudx, cloudy, 'o')
+plt.ylim(-1.2, 1.2)
+plt.xlim(-1.2, 1.2)
+plt.title("Point Cloud", size = 24, weight = 'bold')
+plt.xlabel("x(t)")
+plt.ylabel("x(t + tau)")
+
+
+################
+# Saving plots #
+################
+fig1.savefig("figures/time-series.png")
+fig2.savefig("figures/point-cloud.png")
+
+
 #################
 #     Test      #
 #################
@@ -152,10 +163,10 @@ def dirac(k, n, x, y, eps1, eps2, xi):
 #simp2 = np.array([0,1,1,0])
 #simp3 = np.array([1,1,1,1])
 #simp4 = np.array([1,1,0,0])
-xcoo = np.array([0,1,1,0])
-ycoo = np.array([0,0,1,1])
-epst = 1.0
-epst2 = 2.0
+#xcoo = np.array([0,1,1,0])
+#ycoo = np.array([0,0,1,1])
+#epst = 1.0
+#epst2 = 2.0
 
 #test1 = diameter(simp1, xcoo, ycoo, epst)
 #print(f"Test 1 is {test1} and should be 0")
@@ -169,27 +180,27 @@ epst2 = 2.0
 #test5 = isface(simp4, simp1)
 #print(f"Test 5 is {test5} and should be 0")
 
-test6 = kcomplex(2, 4)
-print(f"Test 6 is \n {test6}")
+#test6 = kcomplex(1, 4)
+#print(f"Test 6 is \n {test6}")
 
-test7 = boundary(2, 4)
-print(f"Test 7 is \n {test7}")
+#test7 = boundary(1, 4)
+#print(f"Test 7 is \n {test7}")
 
-test8 = projection(2, 4, xcoo, ycoo, epst)
-print(f"Test 8 is \n {test8}")
+#test8 = projection(1, 4, xcoo, ycoo, epst)
+#print(f"Test 8 is \n {test8}")
 
-test9 = dirac(2, 4, xcoo, ycoo, epst, epst2, 1)
-print(f"Test 9 is \n {test9}")
-test10, _ = LA.eig( test9 )
-print(f"Test 10 is \n {test10}")
-test11 = np.sum( np.absolute(test10 - 1.0) < 0.001 )
-print(f"The number of loops that persist from scale {epst} to scale {epst2} is:\n {test11}")
+#test9 = dirac(1, 4, xcoo, ycoo, epst, epst2, 1)
+#print(f"Test 9 is \n {test9}")
+#test10, _ = LA.eig( test9 )
+#print(f"Test 10 is \n {test10}")
+#test11 = np.sum( np.absolute(test10 - 1.0) < 0.001 )
+#print(f"The number of loops that persist from scale {epst} to scale {epst2} is:\n {test11}")
 
-test12 = dirac(2, 4, xcoo, ycoo, epst, epst, 1)
-print(f"Test 12 is \n {test12}")
-test13, _ = LA.eig( test12 )
-print(f"Test 13 is \n {test13}")
-test14 = np.sum( np.absolute(test13 - 1.0) < 0.001 )
-print(f"The number of loops that persist from scale {epst} to scale {epst} is:\n {test14}")
+#test12 = dirac(1, 4, xcoo, ycoo, epst, epst, 1)
+#print(f"Test 12 is \n {test12}")
+#test13, _ = LA.eig( test12 )
+#print(f"Test 13 is \n {test13}")
+#test14 = np.sum( np.absolute(test13 - 1.0) < 0.001 )
+#print(f"The number of loops that persist from scale {epst} to scale {epst} is:\n {test14}")
 
 
