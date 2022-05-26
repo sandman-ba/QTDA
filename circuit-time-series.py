ee76d4f80f8a576c744d6a190ad08656659be3dc
sandman <bameneyr@vols.utk.edu>
2022-05-26 18:38:46 -0400

    Worked on quantum circuit to plot graphs of probability.

--- circuit-time-series.py
+++ circuit-time-series.py
@@ -51,18 +51,67 @@ dirac2 = np.block([[ dirac2, np.zeros((12, 4))], [np.zeros((4, 12)), np.zeros((4
 #     Phase estimation       #
 ##############################
 
-had1 = H(8, [0, 1, 2, 6, 7])
-cx1 = CX(8, 2, 5) @ (CX(8, 1, 4) @ CX(8, 0, 3))
-ub1 = UB(m1, l1, dirac1)
-qft1 = QFT(6, m1)
+state1 = np.zeros((2**8, 1), dtype = 'complex_')
+state1[0,0] = 1
+p1 = np.array(range(0,2**8, 2**m1))
+prob1 = np.zeros((2**m1, 1))
 
-circuit1 = qft1 @ (ub1 @ (cx1 @ had1))
+state1 = H(8, [0, 1, 2, 6, 7]) @ state1
+state1 = CX(8, 0, 3) @ state1
+state1 = CX(8, 1, 4) @ state1
+state1 = CX(8, 2, 5) @ state1
+state1 = UB(m1, l1, dirac1) @ state1
+state1 = QFT(6, m1) @ state1
+
+for i in range(2**m1):
+    prob1[i,0] = np.vdot(state1[p1 + i, 0], state1[p1 + i, 0])
 
 
-had2 = H(9, [0, 1, 2, 4, 8])
-cx2 = CX(9, 3, 7) @ (CX(9, 2, 6) @ (CX(9, 1, 5) @ CX(9, 0, 4)))
-ub2 = UB(m2, l2, dirac2)
-qft2 = QFT(8, m2)
+state2 = np.zeros((2**9, 1), dtype = 'complex_')
+state2[0,0] = 1
+p2 = np.array(range(0,2**9, 2**m2))
+prob2 = np.zeros((2**m2, 1))
 
-circuit2 = qft2 @ (ub2 @ (cx2 @ had2))
+state2 = H(9, [0, 1, 2, 4, 8]) @ state2
+state2 = CX(9, 0, 4) @ state2
+state2 = CX(9, 1, 5) @ state2
+state2 = CX(9, 2, 6) @ state2
+state2 = CX(9, 3, 7) @ state2
+state2 = UB(m2, l2, dirac2) @ state2
+state2 = QFT(8, m2) @ state2
+
+for i in range(2**m2):
+    prob2[i,0] = np.vdot(state2[p2 + i, 0], state2[p2 + i, 0])
+
+
+    
+########################
+# Plotting scale 1     #
+########################
+fig1 = plt.figure()
+plt.bar(range(2**m1), 8*prob1[:,0])
+plt.ylim(0.0, 8.0)
+plt.xlim(-0.5, 2**m1 + 1)
+plt.title("Probability at e1", size = 24, weight = 'bold')
+plt.xlabel("lambda*l")
+plt.ylabel("P(lambda*l)")
+
+
+#########################
+# Plotting scale 1 to 2 #
+#########################
+fig2 = plt.figure()
+plt.bar(range(2**m2), 12*prob2[:,0])
+plt.ylim(0.0, 12.0)
+plt.xlim(-0.5, 2**m2 + 1)
+plt.title("Probability from e1 to e2", size = 24, weight = 'bold')
+plt.xlabel("lambda*l")
+plt.ylabel("P(lambda*l)")
+
+
+################
+# Saving plots #
+################
+fig1.savefig("figures/prob-e1.png")
+fig2.savefig("figures/prob-e1-e2.png")
 
