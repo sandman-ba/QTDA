3a1da4f120aa1f722bb72b80db15c06cdc8dd335
Bernardo Ameneyro <bameneyr@vols.utk.edu>
2022-06-12 19:57:23 -0400

    Created script for dirac operator of two squares

---
+++ dirac-two-squares.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from quantumTakens import *
+from classicTakens import *
+
+##############
+# Parameters #
+##############
+l1 = 2
+m1 = 3
+xi = 1.0 # Parameter for Dirac operator
+s = np.sqrt(2)
+xcoo = np.array([0.0, 1.0, 1.0, 0.0, 10.0, 10.0 + s, 10.0 + s, 10.0])
+ycoo = np.array([0.0, 0.0, 1.0, 1.0, 0.0, 0.0, s, s])
+#xcoo = np.array([0.0, 1.0, 1.0, 0.0])
+#ycoo = np.array([0.0, 0.0, 1.0, 1.0])
+
+##############################
+#      Dirac Operators       #
+##############################
+
+dirac1 = dirac(1, 8, xcoo, ycoo, 1.1, 1.5, xi)
+#dirac1 = dirac(1, 4, xcoo, ycoo, 1.1, 1.1, xi)
+print(f"{dirac1}")
