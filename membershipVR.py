0b8795bece413fd3a820139522c5b8b9e62413e8
Bernardo Ameneyro <bameneyr@vols.utk.edu>
2022-07-28 23:59:28 -0400

    Split membership functions into point cloud and takens

--- membershipVR.py
+++ membershipVR.py
@@ -2,33 +2,36 @@ import numpy as np
 from numpy import linalg as LA
 
 # Distance Oracle Point Cloud
-def distanceOracle(data, i, j, eps, mode=None):
-    if mode is None:
-        if (np.abs(data[i] - data[j]) > eps):
-            return 0
-        return 1
-    else:
-        if (LA.norm( data[:,i] - data[:,j]) > eps):
-            return 0
+def distanceOracle(data, i, j, eps):
+    if (LA.norm( data[:,i] - data[:,j]) > eps):
+        return 0
     return 1
 
-# Memebrship Oracle
-def memebrshipOracle(simplex, data, eps, tau=None, d=2):
-    if tau is None:
-        for i, vi in enumerate(simplex):
-            if (vi > 0.5):
-                for j, vj in enumerate(simplex[i:]):
-                    if (vj > 0.5):
-                        if (distanceOracle(data, i, j, eps) < 0.5):
+# Distance Oracle Time Series
+def distanceOracleTakens(data, t, s, eps):
+    if (np.abs(data[t] - data[s]) > eps):
+        return 0
+    return 1
+
+
+# Memebrship Oracle for Point Clouds
+def memebrshipOracle(simplex, data, eps):
+    for i, vi in enumerate(simplex):
+        if vi == 1:
+            for j, vj in enumerate(simplex[i:]):
+                if vj == 1:
+                    if distanceOracle(data, i, j, eps) == 0:
+                        return 0
+    return 1
+
+# Memebrship Oracle for Time Series
+def memebrshipOracleTakens(simplex, data, eps, tau, d):
+    for i, vi in enumerate(simplex):
+        if vi == 1:
+            for j, vj in enumerate(simplex[i:]):
+                if vj == 1:
+                    for t in range(d):
+                        if distanceOracle(data, i + (t * tau), j + (t * tau), eps) == 0:
                             return 0
-        return 1
-    else:
-        for i, vi in enumerate(simplex):
-            if (vi > 0.5):
-                for j, vj in enumerate(simplex[i:]):
-                    if (vj > 0.5):
-                        for t in range(d):
-                            if (distanceOracle(data, i + (t * tau), j + (t * tau), eps) < 0.5):
-                                return 0
-        return 1
+    return 1
 
