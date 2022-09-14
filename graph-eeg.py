2ee559175cc50160b22f22a3feb5c05c409457fc
Bernardo Ameneyro <bameneyr@vols.utk.edu>
2022-09-14 15:16:00 -0400

    Changed font size of double graphs
    Changed titles of simplices graphs

--- graph-eeg.py
+++ graph-eeg.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib as mpl
 import pandas as pd
 import matplotlib.pyplot as plt
 # from statsmodels.graphics.tsaplots import plot_acf
@@ -28,31 +29,32 @@ cloudx = data.Channel2[:points] # Point Cloud x
 cloudy = data.Channel2[tau:] # Point Cloud y
 
 
-########################
-# Plotting Time series #
-########################
-fig1, (ax1, ax2) = plt.subplots(1, 2, figsize = (10, 5.5))
-ax1.plot(data.time[110:150], data.Channel2[110:150], '-')
-ax1.plot(data.time[110:150], data.Channel2[110:150], 'o')
-ax1.set_title("(a)")
-ax1.set_xlabel(r"\(t\)")
-ax1.set_ylabel(r"\(x(t)\)")
+with mpl.rc_context({'font.size': 26}):
+    ########################
+    # Plotting Time series #
+    ########################
+    fig1, (ax1, ax2) = plt.subplots(1, 2, figsize = (10, 5.5))
+    ax1.plot(data.time[110:150], data.Channel2[110:150], '-')
+    ax1.plot(data.time[110:150], data.Channel2[110:150], 'o')
+    ax1.set_title("(a)")
+    ax1.set_xlabel(r"\(t\)")
+    ax1.set_ylabel(r"\(x(t)\)")
 
 
-########################
-# Plotting Point Cloud #
-########################
-ax2.plot(cloudx[110:150], cloudy[110:150], 'o')
-ax2.set_title("(b)")
-ax2.set_xlabel(r"\(x(t)\)")
-ax2.set_ylabel(r"\(x(t + \tau)\)")
+    ########################
+    # Plotting Point Cloud #
+    ########################
+    ax2.plot(cloudx[110:150], cloudy[110:150], 'o')
+    ax2.set_title("(b)")
+    ax2.set_xlabel(r"\(x(t)\)")
+    ax2.set_ylabel(r"\(x(t + \tau)\)")
 
-fig1.set_tight_layout(True)
+    fig1.set_tight_layout(True)
 
-################
-# Saving plots #
-################
-fig1.savefig("figures/eeg.png")
+    ################
+    # Saving plots #
+    ################
+    fig1.savefig("figures/eeg.png")
 
 ############################
 # Plotting autocorrelation #
--- graph-time-series.py
+++ graph-time-series.py
@@ -27,38 +27,38 @@ cloudx = series[:points] # Point Cloud x
 cloudy = series[tau:] # Point Cloud y
 
 
-########################
-# Plotting Time series #
-########################
-fig1, (ax1, ax2) = plt.subplots(1, 2, figsize = (10, 5.5))
-time2 = np.linspace(0.0, 2.0, 100)
-ax1.plot(time2, f(time2), '-')
-ax1.plot(time, series, 'o')
-ax1.set_title("(a)")
-ax1.set_xlabel(r"\(t\)")
-ax1.set_ylabel(r"\(x(t)\)")
-
-
-########################
-# Plotting Point Cloud #
-########################
-ax2.plot(cloudx, cloudy, 'o')
-ax2.set_title("(b)")
-ax2.set_xlabel(r"\(x(t)\)")
-ax2.set_ylabel(r"\(x(t + \tau)\)")
-
-fig1.set_tight_layout(True)
-
-################
-# Saving plots #
-################
-fig1.savefig("figures/periodic-function.png")
-
-
-######################
-# Plotting simplices #
-######################
 with mpl.rc_context({'font.size': 26}):
+    ########################
+    # Plotting Time series #
+    ########################
+    fig1, (ax1, ax2) = plt.subplots(1, 2, figsize = (10, 5.5))
+    time2 = np.linspace(0.0, 2.0, 100)
+    ax1.plot(time2, f(time2), '-')
+    ax1.plot(time, series, 'o')
+    ax1.set_title("(a)")
+    ax1.set_xlabel(r"\(t\)")
+    ax1.set_ylabel(r"\(x(t)\)")
+
+
+    ########################
+    # Plotting Point Cloud #
+    ########################
+    ax2.plot(cloudx, cloudy, 'o')
+    ax2.set_title("(b)")
+    ax2.set_xlabel(r"\(x(t)\)")
+    ax2.set_ylabel(r"\(x(t + \tau)\)")
+
+    fig1.set_tight_layout(True)
+
+    ################
+    # Saving plots #
+    ################
+    fig1.savefig("figures/periodic-function.png")
+
+
+    ######################
+    # Plotting simplices #
+    ######################
     fig2, (ax3, ax4) = plt.subplots(1, 2, figsize = (10, 5.5))
 
 
@@ -84,7 +84,7 @@ with mpl.rc_context({'font.size': 26}):
     ax3.plot(cloudx, cloudy, 'or')
     ax3.set_xlim(-2.0, 2.0)
     ax3.set_ylim(-2.0, 2.0)
-    ax3.set_title(fr"Scale \(\epsilon_1 = {e1}\)")
+    ax3.set_title(fr"(a)")
     ax3.set_xlabel(r"\(x(t)\)")
     ax3.set_ylabel(r"\(x(t + \tau)\)")
 
@@ -98,12 +98,12 @@ with mpl.rc_context({'font.size': 26}):
     ax4.set_xlim(-2.0, 2.0)
     ax4.set_ylim(-2.0, 2.0)
     ax4.set_yticks([])
-    ax4.set_title(fr"Scale \(\epsilon_2 = {e2}\)")
+    ax4.set_title(fr"(b)")
     ax4.set_xlabel(r"\(x(t)\)")
 
     fig2.set_tight_layout(True)
 
-################
-# Saving plots #
-################
+    ################
+    # Saving plots #
+    ################
     fig2.savefig("figures/simplices.png")
