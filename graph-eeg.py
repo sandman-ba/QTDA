b34164c9b86dfcf764790f616bb0ac71d4f7b4e5
Bernardo Ameneyro <bameneyr@vols.utk.edu>
2022-09-14 14:59:17 -0400

    Joined time series and point cloud graphs for EEG

--- graph-eeg.py
+++ graph-eeg.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from statsmodels.graphics.tsaplots import plot_acf
+# from statsmodels.graphics.tsaplots import plot_acf
 from numpy import pi
 
 
@@ -31,11 +31,10 @@ cloudy = data.Channel2[tau:] # Point Cloud y
 ########################
 # Plotting Time series #
 ########################
-fig1, ax1 = plt.subplots(1, 1, figsize = (6.5, 5))
-#ax1.plot(data.time[100:200], data.Channel2[100:200], '-')
+fig1, (ax1, ax2) = plt.subplots(1, 2, figsize = (10, 5.5))
 ax1.plot(data.time[110:150], data.Channel2[110:150], '-')
 ax1.plot(data.time[110:150], data.Channel2[110:150], 'o')
-#ax1.set_title("Time Series")
+ax1.set_title("(a)")
 ax1.set_xlabel(r"\(t\)")
 ax1.set_ylabel(r"\(x(t)\)")
 
@@ -43,34 +42,31 @@ ax1.set_ylabel(r"\(x(t)\)")
 ########################
 # Plotting Point Cloud #
 ########################
-fig2, ax2 = plt.subplots(1, 1, figsize = (6.5, 5))
 ax2.plot(cloudx[110:150], cloudy[110:150], 'o')
-#ax2.set_title("Point Cloud")
+ax2.set_title("(b)")
 ax2.set_xlabel(r"\(x(t)\)")
 ax2.set_ylabel(r"\(x(t + \tau)\)")
 
 fig1.set_tight_layout(True)
-fig2.set_tight_layout(True)
 
 ################
 # Saving plots #
 ################
-fig1.savefig("figures/time-series-eeg.png")
-fig2.savefig("figures/point-cloud-eeg.png")
+fig1.savefig("figures/eeg.png")
 
 ############################
 # Plotting autocorrelation #
 ############################
 
-fig3, ax3 = plt.subplots(1, 1, figsize = (6.5, 5))
-pd.plotting.autocorrelation_plot(data.Channel2[110:150], ax3)
-ax3.set_xlim([0, 10])
+# fig3, ax3 = plt.subplots(1, 1, figsize = (6.5, 5))
+# pd.plotting.autocorrelation_plot(data.Channel2[110:150], ax3)
+# ax3.set_xlim([0, 10])
 
-fig3.set_tight_layout(True)
-fig3.savefig("figures/autocorrelation-eeg.png")
+# fig3.set_tight_layout(True)
+# fig3.savefig("figures/autocorrelation-eeg.png")
 
-fig4, ax4 = plt.subplots(1, 1, figsize = (6.5, 5))
-plot_acf(data.Channel2[110:150], ax4, lags=10)
+# fig4, ax4 = plt.subplots(1, 1, figsize = (6.5, 5))
+# plot_acf(data.Channel2[110:150], ax4, lags=10)
 
-fig4.set_tight_layout(True)
-fig4.savefig("figures/autocorrelation2-eeg.png")
+# fig4.set_tight_layout(True)
+# fig4.savefig("figures/autocorrelation2-eeg.png")
